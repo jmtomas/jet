d94d279532c7933e3f3b785d8e72c487757c9efe
Juan Manuel Tomás <jtomas1815@gmail.com>
2020-03-25 06:12:47 -0300

    Read command line argument into memory

--- jet.c
+++ jet.c
@@ -5,8 +5,17 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+	char *buffer = 0;
 	if (argc > 1) {
 		int file_descriptor = open(argv[1], O_RDWR);
+
+		struct stat file_stat;
+		stat(argv[1], &file_stat);
+		size_t file_size = file_stat.st_size;
+
+		buffer = malloc(file_size);
+		read(file_descriptor, buffer, file_size);
+
 		close(file_descriptor);
 	}
 	return 0;
