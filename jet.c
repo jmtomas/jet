8e13a76cbbafda83ab258c24c7bbee128c293800
Juan Manuel Tomás <jtomas1815@gmail.com>
2020-03-25 06:29:30 -0300

    Add basic error checking

--- jet.c
+++ jet.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -8,15 +9,17 @@ int main(int argc, char *argv[]) {
 	char *buffer = 0;
 	if (argc > 1) {
 		int file_descriptor = open(argv[1], O_RDONLY);
+		assert(file_descriptor != -1);
 
 		struct stat file_stat;
-		stat(argv[1], &file_stat);
+		assert(stat(argv[1], &file_stat) != -1);
 		size_t file_size = file_stat.st_size;
 
 		buffer = malloc(file_size);
-		read(file_descriptor, buffer, file_size);
+		assert(buffer);
+		assert(read(file_descriptor, buffer, file_size) != -1);
 
-		close(file_descriptor);
+		assert(close(file_descriptor) != -1);
 	}
 	return 0;
 }
