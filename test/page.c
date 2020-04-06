e7117322149fdc0196acb49c2ae0ff32e2c299c0
Juan Manuel Tomás <jtomas1815@gmail.com>
2020-04-06 00:07:32 -0300

    Fix text not rendering until first key press

--- test/page.c
+++ test/page.c
@@ -14,6 +14,18 @@ int main() {
 	keypad(stdscr, TRUE);
 
 	while (!exit) {
+		int gap_start = p->gap_start - p->buffer;
+		int gap_end = p->gap_end - p->buffer;
+		for (int i = 0; i < gap_start; i++) {
+			addch(p->buffer[i]);
+		}
+		for (int i = gap_start; i < gap_end; i++) {
+			addch('_');
+		}
+		for (int i = gap_end; i < PAGE_SIZE; i++) {
+			addch(p->buffer[i]);
+		}
+
 		int input = getch();
 		clear();
 		switch (input) {
@@ -28,17 +40,6 @@ int main() {
 				break;
 			default:
 				insert_into_page(p, input);
-		}
-		int gap_start = p->gap_start - p->buffer;
-		int gap_end = p->gap_end - p->buffer;
-		for (int i = 0; i < gap_start; i++) {
-			addch(p->buffer[i]);
-		}
-		for (int i = gap_start; i < gap_end; i++) {
-			addch('_');
-		}
-		for (int i = gap_end; i < PAGE_SIZE; i++) {
-			addch(p->buffer[i]);
 		}
 	}
 
