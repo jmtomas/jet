352c3394a7189f01f455bcbf83d2bf2a8641292d
Juan Manuel Tomás <jtomas1815@gmail.com>
2020-04-07 21:28:57 -0300

    Show contents of loaded file

--- jet.c
+++ jet.c
@@ -4,16 +4,18 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <curses.h>
 
 int main(int argc, char *argv[]) {
 	char *buffer = 0;
+	size_t file_size = 0;
 	if (argc > 1) {
 		int file_descriptor = open(argv[1], O_RDONLY);
 		assert(file_descriptor != -1);
 
 		struct stat file_stat;
 		assert(stat(argv[1], &file_stat) != -1);
-		size_t file_size = file_stat.st_size;
+		file_size = file_stat.st_size;
 
 		buffer = malloc(file_size);
 		assert(buffer);
@@ -21,5 +23,22 @@ int main(int argc, char *argv[]) {
 
 		assert(close(file_descriptor) != -1);
 	}
+
+	initscr();
+	cbreak();
+	noecho();
+	nonl();
+	intrflush(stdscr, FALSE);
+	keypad(stdscr, TRUE);
+
+	while (1) {
+		for (int i = 0; i < file_size; i++) {
+			addch(buffer[i]);
+		}
+		getch();
+		clear();
+	}
+
+	endwin();
 	return 0;
 }
