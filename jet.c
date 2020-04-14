e10c32d01e8416923226f211dd855824a8123dcd
Juan Manuel Tomás <jtomas1815@gmail.com>
2020-04-14 00:39:04 -0300

    Refactor jet.c

--- jet.c
+++ jet.c
@@ -33,16 +33,18 @@ int main(int argc, char *argv[]) {
 
 	int window_height = getmaxy(stdscr);
 	int current_line = 0;
+	int old_line = -1;
+
 	int number_of_lines = 0;
 	for (int i = 0; i < file_size; i++) {
 		if (buffer[i] == '\n') number_of_lines++;
 	}
-	int old_line = -1;
+
 	while (1) {
 		if (old_line != current_line) {
 			clear();
-			int iter_line = 0;
-			for (int i = 0; i < file_size && iter_line < window_height + current_line; i++) {
+
+			for (int i = 0, iter_line = 0; i < file_size && iter_line < window_height + current_line; i++) {
 				if (iter_line >= current_line) addch(buffer[i]);
 				if (buffer[i] == '\n') iter_line++;
 			}
